c121cdabd3eaf4157f0f692977f6dadc890fbffe
Fahad Hasan <66886981+hasan-fahad@users.noreply.github.com>
2023-05-04 19:39:55 +0600

    firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBFJgWP-tKKbY5P76ivPfvK3EwHgLWFtjA",
-  authDomain: "chef-recipe-hunting-a01c9.firebaseapp.com",
-  projectId: "chef-recipe-hunting-a01c9",
-  storageBucket: "chef-recipe-hunting-a01c9.appspot.com",
-  messagingSenderId: "653050182269",
-  appId: "1:653050182269:web:c257673f63ba8968b74f14",
-  measurementId: "G-YDMZYW79FB"
+  apiKey: import.meta.VITE_APIKEY,
+  authDomain: import.meta.VITE_AUTHDOMAIN,
+  projectId: import.meta.VITE_PROJECTED,
+  storageBucket: import.meta.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.VITE_MESSAGINGSENDERID,
+  appId: import.meta.VITE_APPID,
+
 };
 
 // Initialize Firebase
